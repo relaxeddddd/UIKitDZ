7163d3009f5f57c2ba9278783279add5dcc3eca7
Nihao1337 <tarchukov.2000@mail.ru>
2024-02-08 19:46:59 +0300

    Добавил правильный приоритет кнопок в alert

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -31,8 +31,8 @@ final class ViewController: UIViewController {
         alertController.addTextField { textField in
             textField.placeholder = "Введите слово"
         }
-        alertController.addAction(UIAlertAction(title: "Отмена", style: .cancel))
-        alertController.addAction(UIAlertAction(title: "Ок", style: .default) { _ in
+        let actionCancle = UIAlertAction(title: "Отмена", style: .default)
+        let actionOk = UIAlertAction(title: "Ок", style: .default) { _ in
             self.myView.startButton.frame = CGRect(
                 x: 19,
                 y: 650,
@@ -44,7 +44,10 @@ final class ViewController: UIViewController {
             self.myView.wordTitleLabelTwo.isHidden = false
             guard let word = alertController.textFields?.first?.text else { return }
             self.myView.newWord.text = self.myModel.reversed(word: word).capitalized
-        })
+        }
+        alertController.addAction(actionCancle)
+        alertController.addAction(actionOk)
+        alertController.preferredAction = actionOk
         present(alertController, animated: true)
     }
 }
